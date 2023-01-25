b48e480162ee55345c3553db8d814e8b7b754220
samra-xloop <samra.almas@xloopdigital.com>
2023-01-25 15:24:38 +0500

    testing done

--- src/main/java/com/xloop/resourceloop/candidateprofile/model/CandidatePersonalInfo.java
+++ src/main/java/com/xloop/resourceloop/candidateprofile/model/CandidatePersonalInfo.java
@@ -1,6 +1,7 @@
 package com.xloop.resourceloop.candidateprofile.model;
 import java.sql.Date;
-import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -11,12 +12,16 @@ import lombok.Setter;
 @Entity
 @Table(name = "candidate_personal_information")
 public class CandidatePersonalInfo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
-    private LocalDate dateOfBirth;
+
+    @Column(columnDefinition = "date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date dateOfBirth;
     private String gender;
     private String nationalIdentityNumber;
     private String phoneNumber;
@@ -24,4 +29,23 @@ public class CandidatePersonalInfo {
     private String address;
     private String linkedProfile;
     private String maritalStatus;
+
+    public CandidatePersonalInfo(String firstName, String lastName, Date dateOfBirth, String gender,
+            String nationalIdentityNumber, String phoneNumber, String city, String address, String linkedProfile,
+            String maritalStatus) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.nationalIdentityNumber = nationalIdentityNumber;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.address = address;
+        this.linkedProfile = linkedProfile;
+        this.maritalStatus = maritalStatus;
+    }
+
+    public CandidatePersonalInfo(){};
+
+    
 }
